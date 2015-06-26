0de33282fa387d4d3bede5a5f831599957f64f2f
herohead049 <herohead@gmail.com>
2015-06-26 17:00:19 -0400

    a

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ var saveGroup = {
     name: "",
     startTime: "",
     endTime: "",
+    restartTime: "",
+    details: [],
     succeeded: [],
     disabled: [],
     failed: []
 }
+
+var temp =  [{
+    server: "",
+    details: []
+}];
 
 
 function findPattern(files, regex) {
@@ -37,9 +44,30 @@ function findPattern(files, regex) {
                 if (match = ln.match("Succeeded:")) {
                     emitter.emit('succeeded',ln.replace(/\s/g, ''), file);
                 }
+
+                if (match = ln.match("Failed:")) {
+                    emitter.emit('failed',ln.replace(/\s/g, ''),file);
+                }
                 if (match = ln.match("Disabled:")) {
                     emitter.emit('disabled',ln.replace(/\s/g, ''),file);
                 }
+                if (match = ln.match("Start time:")) {
+                    emitter.emit('startTime',ln,file);
+                }
+                if (match = ln.match("Restart time:")) {
+                    emitter.emit('restartTime',ln,file);
+                }
+                if (match = ln.match("End time:")) {
+                    emitter.emit('endTime',ln,file);
+                }
+                if (match = ln.match("NetWorker savegroup:")) {
+                    emitter.emit('title',ln,file);
+                }
+                if (match = ln.match(/\* /)) {
+                    emitter.emit('details',ln,file);
+                }
+
+
 
             });
 
@@ -53,12 +81,13 @@ function findPattern(files, regex) {
 
 
 findPattern(
-['sgfiles/test1.txt', 'sgfiles/test2.txt'],
+['sgfiles/test1.txt'],
     /\(notice\)/g
     )
 .on('fileread', function(file) {
     console.log(file + ' was read');
     console.log(saveGroup);
+    console.log(temp);
 })
 .on('found', function(file, match) {
     console.log('Matched "' + match + '" in file ' + file);
@@ -67,13 +96,31 @@ findPattern(
     console.log('Error emitted: ' + err.message);
 })
 .on('disabled', function(ln, file) {
-    //console.log('Matched "' + match + '" in file ' + file);
-    //console.log(error(ln),file);
     saveGroup.disabled = ln.split(":")[1].split(",");
 })
+.on('startTime', function(ln, file) {
+    saveGroup.startTime = ln.split("time:")[1].trim();
+})
+.on('endTime', function(ln, file) {
+    saveGroup.endTime = ln.split("time:")[1].trim();
+})
+.on('restartTime', function(ln, file) {
+    saveGroup.restartTime = ln.split("time:")[1].trim();
+})
+.on('title', function(ln, file) {
+    saveGroup.name = ln.split(":")[1].split(",")[0].trim().split(" ")[1];
+})
+.on('failed', function(ln, file) {
+    saveGroup.failed = ln.split(":")[1].split(",");
+})
+.on('details', function(ln, file) {
+    var t = ln.split(":")[0].split(" ")[1];
+    //console.log(t);
+    temp.server[t] = t;
+    //temp.details.push(ln);
+
+})
 .on('succeeded', function(ln, file) {
-    //console.log('Matched "' + match + '" in file ' + file);
-    //console.log(success(ln),file);
     saveGroup.succeeded = ln.split(":")[1].split(",");
 });
 
